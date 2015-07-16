4534e04a45f75af30c9d4e760a8f9175b3aca18d
RafaelKozar <rafael_kozar@hotmail.com>
2015-07-16 09:09:48 -0300

    commit

--- app.js
+++ app.js
@@ -1,88 +1,232 @@
-/**
- * Module dependencies.
- */
 
 var express = require('express');
-var app = require("./server.js");
-var  fs = require('fs');
-var cv   = require('cloudcv-backend');
 
-app.use(express.errorHandler());
+var mongoose = require('mongoose');
+var flash = require('connect-flash');
+var passport = require('passport');
+
+var morgan = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+
+var configDB = require('./config/database.js');
+
+mongoose.connect(configDB.url);
+require('./config/passport')(passport); // pass passport for configuration
+
+var fs = require('fs');
+var cv = require('cloudcv-backend');
+var request = require('request');
+var NodeCache = require("node-cache");
+var myCache = new NodeCache();
+
+var http = require('http');
+var path = require('path');
+
+var appSet = express();
+var util = require('util');
+
+
 
 var io = require('socket.io');
-var server = require('http').createServer(app);
-server.listen(8888, 'localhost');
+var server = require('http').createServer(appSet);
+//var api = require('./routes/api.js');
+//var routes = require('./routes');
 
+appSet.set('views', __dirname + '/views');
+appSet.engine('html', require('ejs').renderFile);
+appSet.set('view engine', 'html');
+appSet.set('view options', { pretty: true });
 
+appSet.use(morgan('dev'));
+appSet.use(cookieParser());
+appSet.use(bodyParser.json()); // get information from html forms
+appSet.use(bodyParser.urlencoded({ extended: true }));
+
+appSet.use(express.favicon());
+//appSet.use(express.logger('dev'));
+
+//appSet.use(express.bodyParser());
+appSet.use(session({ secret: 'ilovescotchscotchyscotchscotch' }));//sesion secret
+appSet.use(passport.initialize());
+appSet.use(passport.session()); // persistent login sessions
+appSet.use(flash()); // use connect-flash for flash messages stored in session
+
+require('./routes/routes.js')(appSet, passport); // load our routes and pass in our app and fully configured passport
+
+//appSet.use(express.methodOverride());
+//appSet.use(appSet.router);
+//appSet.use(express.static(path.join(__dirname, 'public')));
+
+/*appSet.get('/', function (req, res)
+{   
+    handleApiRequest("dominantColors", "http://104.131.163.197:3000/images/test.png");
+    res.render('index');
+});
+
+appSet.get('/login', function (req, res) {
+    res.render('login');
+})
+
+appSet.get('/adm', function (req, res) {
+    res.render('adm');
+})
+
+appSet.get('/cadastrouserrobo', function (req, res) {
+    res.render('cadastrouserrobo');
+})
+
+appSet.get('/imagem', function (req, res) {    
+    res.render('imagem');
+});
+
+*/
+
+server.listen(3000);
 io = io.listen(server);
 
-
+var global;
 var visitas = 0;
 
-/*io.sockets.on('connection', function(socket) {
- // do all of your socket work in here
- console.emit('message', "MSG");
- //    socket.broadcast.emit("funcao", visitas);
- });
- 
- */
-io.sockets.on('connection', function(socket) {
-    visitas++;
-    // Envia o total de visitas para o novo usuário.
-    socket.emit('visits', visitas);
-    // Envia o total de visitas para os demais usuários.
-    socket.broadcast.emit('visits', visitas);
+var filters = {};
 
-    /*socket.on('enviar', function(msg) {
-       console.log(msg);
-       //sonsole.log("\n");
-    });*/
-    socket.on('enviar', function(img) {
-        data = img.replace(/^data:image\/png;base64,/, '');    
+filters.histogram = function (image, callback) {
+    try {
         
-        cv.analyzeImage(data, function(error, result) {
-            
+        cv.analyzeImage(image, function (error, result) {
             if (error) {
-                //response.statusCode = 415; // Unsupported Media Type
-                //response.end();
+                
                 return;
             }
             
             callback(null, result.histogram);
         });
-                
-        var filePath = __dirname + '/test.png';        
+    }
+    catch (e) {
+        console.error(e);
         
+        callback(null);
+    }
+};
+
+filters.dominantColors = function (image, callback) {
+    
+    try {
+        console.log("entramos na func");
+        cv.analyzeImage(image, function (error, result) {
+            if (error) {
+                console.log(error);
+                console.log("analyze");
+                return;
+            }
+            
+            callback(null, result.intensity);
+            console.log(result.intensity);
+        });
+    }
+    catch (e) {
+        console.error(e);
         
-         fs.writeFile(filePath, data, 'base64', function(err) {
-            if (err) {
-                console.log('! Error saving PNG: ' + err);
-                //res.json(200, { error: 'Error saving PNG: ' + err });
-            } else {
-                console.log('> PNG file saved to: ' + filePath);
-                //res.json(200, { success: 'PNG file saved to: ' + filePath });
+        callback(e);
+    }
+};
+
+
+
+function handleApiRequest(filter, url) {
+    
+    console.log("API request", filter, url);
+    
+    // Construct request data
+    var requestSettings = {
+        method: 'GET',
+        url: url,
+        encoding: null
+    };
+    
+    var handler = filters[filter];
+    console.log(typeof handler);
+    
+    if (typeof handler !== 'function') {
+        //res.end();
+        console.log("!=funct");
+    }
+    else {
+        var key = filter + url;
+        
+        myCache.get(key, function (err, value) {
+            console.log(value);
+            if (!err && value[key]) {
+                //res.setHeader("Content-Type", "application/json");
+                //res.send(value[key]);
+                console.log("vish");
+                console.log(value[key]);
+            }
+            else {
+                console.log("11");
+                request(requestSettings, 
+                    function (error, response, body) {
+                    console.log("22");
+                    if (!error && response.statusCode == 200) {
+                        console.log("33");
+                        console.log(body);
+                        handler(body, function (error, cachedResponse) {
+                            console.log("34");
+                            //  if (!error)
+                            //      myCache.set(key, cachedResponse);
+                            console.log("44");
+                            //res.setHeader("Content-Type", "application/json");
+                            //res.write(JSON.stringify(cachedResponse));
+                            //res.end();
+                        });
+                    }
+                    else {
+                        console.log("Error loading " + url);
+                        // res.end();
+                    }
+                });
             }
         });
+
+
+    }
+};
+
+//io.sockets.on('conectar', function (socket){
+//    socket
+//};
+
+var visitas = 0;
+
+io.sockets.on('connection', function (socket) {
+    visitas++;
+    // Envia o total de visitas para o novo usuário.
+    socket.emit('visits', visitas);
+    // Envia o total de visitas para os demais usuários.
+    socket.broadcast.emit('visits', visitas);
+    
+    socket.on('enviar', function (img) {
+        data = img.replace(/^data:image\/png;base64,/, '');
+        
+        var filePath = '../public/images/test.png';
+        
+        fs.writeFile(filePath, data, 'base64', function (err) {
+            if (err) {
+                console.log('! Error saving PNG: ' + err);
+            } else {
+                console.log('> PNG file saved to: ' + filePath);
+            }
+        });
+        
+        
+        handleApiRequest("dominantColors", "http://localhost:3000/images/test.png");
+            
     });
-    //console.log(socket);
-    // Evento disconnect ocorre quando sai um usuário.
-    socket.on('disconnect', function() {
-        //  visitas--;
+    
+    socket.on('disconnect', function () {
+        visitas--;
         // Atualiza o total de visitas para os demais usuários.
         socket.broadcast.emit('message', visitas);
     });
 });
-
-
-/*io.sockets.on('connection', function(socket){  
- //can.width  = 200;
- //can.height = 200;
- console.log("1");
- socket.on('fvideo', function(stream){
- // can.getContext('2d').drawImage(stream, 0, 0, can.width, can.height); 
- //var dataUrl = can.toDataURL("image/jpeg");             
- console.log("2"); 
- io.sockets.emit('frame', "msg");            
- });
- 
- });*/
---
+++ routes/routes.js
@@ -0,0 +1,69 @@
+module.exports = function (app, passport) {
+
+    // normal routes ===============================================================
+
+    // show the home page (will also have our login links)
+    app.get('/', isLoggedIn, function (req, res) {
+        res.render('index');
+    });
+
+    app.get('/adm', function (req, res) {
+        res.render('adm');
+    })
+
+    app.get('/cadastrouserrobo', function (req, res) {
+        res.render('cadastrouserrobo');
+    })
+
+
+    app.get('/signup', function (req, res) {
+        res.render('signup', { message: req.flash('signupMessage') });
+    });
+
+    // process the signup form
+    app.post('/signup', passport.authenticate('local-signup', {
+        successRedirect: '/', // redirect to the secure profile section
+        failureRedirect: '/signup', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
+
+    // LOGOUT ==============================
+    app.get('/logout', function (req, res) {
+        req.logout();
+        res.redirect('/login');
+    });
+
+    // show the login form
+    app.get('/login', function (req, res) {
+        res.render('login', { message: req.flash('loginMessage') });
+    });
+
+    // process the login form
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/', // redirect to the secure profile section
+        failureRedirect: '/login', // redirect back to the signup page if there is an error
+        failureFlash: true // allow flash messages
+    }));
+
+
+
+    // local -----------------------------------
+    app.get('/unlink/local', isLoggedIn, function (req, res) {
+        var user = req.user;
+        user.local.email = undefined;
+        user.local.password = undefined;
+        user.save(function (err) {
+            res.redirect('/login');
+        });
+    });
+
+
+};
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.redirect('/login');
+}
