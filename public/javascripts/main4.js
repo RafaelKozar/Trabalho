591eb1a73fb313d7578889f9792c554ef31aa5f7
RafaelKozar <rafael_kozar@hotmail.com>
2015-12-01 09:55:04 -0200

    teste 3

--- public/javascripts/main4.js
+++ public/javascripts/main4.js
@@ -31,8 +31,8 @@ document.addEventListener('DOMContentLoaded', function () {
     var makePara = function (text) {
         var p = document.createElement('p');
         p.innerText = text;
-        //return p;
-        return "";
+        return p;
+        //return "";
     };
     
     var addMessage = function (para) {
@@ -47,7 +47,7 @@ document.addEventListener('DOMContentLoaded', function () {
     var logError = function (text) {
         var p = makePara('ERROR: ' + text);
         p.style.color = 'red';
-        p = "";
+        //p = "";
         addMessage(p);
     };
     
