e7c0016f7e23af3a3e45cc3b94aff506709304ee
RafaelKozar <rafael_kozar@hotmail.com>
2015-12-03 16:44:52 -0200

    vai

--- public/javascripts/meumain3.js
+++ public/javascripts/meumain3.js
@@ -189,6 +189,7 @@
             
             var call = peer.call(recipientId, stream);
             
+            sleep(1000);
             call.on('stream', showRemoteStream);
             
             call.on('error', function (e) {
